c60de90311762747873fe382c02d4f40b45a64bd
Cecília Maria Soares Araujo <cecilia.ma@dinfor.uniceub>
2024-05-20 08:27:22 -0300

    novas atividades

---
+++ PlaygroundAndroidCompose/app/src/main/java/br/com/ceub/projeto/playgroundandroidcompose/list/model/Paisagem.kt
@@ -0,0 +1,9 @@
+package br.com.ceub.projeto.playgroundandroidcompose.list.model
+
+import androidx.annotation.DrawableRes
+
+data class Paisagem(
+    @DrawableRes val idImg : Int,
+    val descricao : String
+)
+
